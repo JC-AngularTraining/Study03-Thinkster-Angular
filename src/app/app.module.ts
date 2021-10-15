968ce1f7e52121b57f9281b3bb94feb3587d59db
Justin <just.coder@outlook.com>
2021-10-15 16:05:38 +0100

    Exercise1 bring in api

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { Exercise1Component } from './local_Apps/route_Pages/exercise1/exercise1
 import { Exercise2Component } from './local_Apps/route_Pages/exercise2/exercise2.component';
 import { Exercise3Component } from './local_Apps/route_Pages/exercise3/exercise3.component';
 import { Exercise4Component } from './local_Apps/route_Pages/exercise4/exercise4.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -32,6 +33,7 @@ import { Exercise4Component } from './local_Apps/route_Pages/exercise4/exercise4
     BrowserAnimationsModule,
     UtilsModule,
     FontAwesomeModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/local_Apps/route_Pages/exercise1/exercise1.component.ts
+++ src/app/local_Apps/route_Pages/exercise1/exercise1.component.ts
@@ -1,15 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { ServExercise1Service } from 'src/app/_services/serv-exercise1.service';
 
 @Component({
   selector: 'app-exercise1',
   templateUrl: './exercise1.component.html',
-  styleUrls: ['./exercise1.component.css']
+  styleUrls: ['./exercise1.component.css'],
 })
 export class Exercise1Component implements OnInit {
-
-  constructor() { }
+  pageTitle?: string;
+  constructor(private api: ServExercise1Service) {}
 
   ngOnInit(): void {
+    this.api.apiCall().subscribe((data) => {
+      console.log(data);
+      this.pageTitle = 'Exercise 1';
+    });
   }
-
 }

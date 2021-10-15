a03074258666cb55be939a3f7d8b689c35fcaa26
Justin <just.coder@outlook.com>
2021-10-15 15:13:30 +0100

    Build app Structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,25 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { Exercise1Component } from './local_Apps/route_Pages/exercise1/exercise1.component';
+import { Exercise2Component } from './local_Apps/route_Pages/exercise2/exercise2.component';
+import { Exercise3Component } from './local_Apps/route_Pages/exercise3/exercise3.component';
+import { Exercise4Component } from './local_Apps/route_Pages/exercise4/exercise4.component';
+import { HomeComponent } from './local_Apps/route_Pages/home/home.component';
+import { PageNotFoundComponent } from './local_Apps/_page-not-found/page-not-found.component';
 
-const routes: Routes = [];
+const appRoutes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'exercise1', component: Exercise1Component },
+  { path: 'exercise2', component: Exercise2Component },
+  { path: 'exercise3', component: Exercise3Component },
+  { path: 'exercise4', component: Exercise4Component },
+
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,37 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+import { UtilsModule } from './local_Apps/__utils/__utils.module';
+import { NavBarComponent } from './local_Apps/_nav-bar/nav-bar.component';
+import { HomeComponent } from './local_Apps/route_Pages/home/home.component';
+import { PageNotFoundComponent } from './local_Apps/_page-not-found/page-not-found.component';
+import { Exercise1Component } from './local_Apps/route_Pages/exercise1/exercise1.component';
+import { Exercise2Component } from './local_Apps/route_Pages/exercise2/exercise2.component';
+import { Exercise3Component } from './local_Apps/route_Pages/exercise3/exercise3.component';
+import { Exercise4Component } from './local_Apps/route_Pages/exercise4/exercise4.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    Exercise1Component,
+    NavBarComponent,
+    HomeComponent,
+    PageNotFoundComponent,
+    Exercise2Component,
+    Exercise3Component,
+    Exercise4Component,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    UtilsModule,
+    FontAwesomeModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}

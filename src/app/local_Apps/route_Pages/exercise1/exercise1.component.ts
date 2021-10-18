3b5493b5c756f6c57c7de62173d3cd511dab59b5
Justin <just.coder@outlook.com>
2021-10-18 10:22:55 +0100

    Complete Exercise One

--- src/app/local_Apps/route_Pages/exercise1/exercise1.component.ts
+++ src/app/local_Apps/route_Pages/exercise1/exercise1.component.ts
@@ -1,3 +1,4 @@
+import { ThrowStmt } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { ServExercise1Service } from 'src/app/_services/serv-exercise1.service';
 
@@ -7,13 +8,38 @@ import { ServExercise1Service } from 'src/app/_services/serv-exercise1.service';
   styleUrls: ['./exercise1.component.css'],
 })
 export class Exercise1Component implements OnInit {
-  pageTitle?: string;
+  pageTitle: string = 'Exercise 1 - Flopvoter';
+  selectedIndex: number = -1;
+
   constructor(private api: ServExercise1Service) {}
 
   ngOnInit(): void {
     this.api.apiCall().subscribe((data) => {
       console.log(data);
-      this.pageTitle = 'Exercise 1';
     });
+  }
+
+  onVoteClick(index: number) {
+    if (this.isSelected(index)) {
+      this.selectedIndex = -1;
+    } else {
+      this.selectedIndex = index;
+    }
+  }
+
+  isSelected(index: number) {
+    return this.selectedIndex === index;
+  }
+
+  getButtonText(index: number) {
+    if (this.isSelected(index)) {
+      return 'Unvote';
+    } else {
+      return 'Vote';
+    }
+  }
+
+  anySelected() {
+    return this.selectedIndex !== -1;
   }
 }
